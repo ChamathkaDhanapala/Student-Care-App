2581c4fb71ccb9ea6e69c11849503909f5557246
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2025-01-05 12:24:46 +0530

    Create components

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <ScrollView>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+    </PaperProvider>
   );
 }
 
