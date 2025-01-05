79542e26bff3a1564049db01e4655a048b7a70e5
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2025-01-05 18:07:10 +0530

    Create logo.js file to include logo

---
+++ MyMobileApp/components/Logo.js
@@ -0,0 +1,13 @@
+import { View, Image } from 'react-native'
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <View>
+      <Image
+      source={require('../assets/images/logo.png')}
+      style={{ width: 305, height: 75, resizeMode: 'contain' }}
+      />
+    </View>
+  )
+}
